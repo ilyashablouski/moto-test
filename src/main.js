34822de07229f5b228f0c8519c6f29dd4d46bdfb
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-08-17 22:03:01 +0300

    create mocked json data & add to nav

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import './styles/main.scss';
 import '@styles/components.styles.js';
 import Alpine from 'alpinejs';
 
+import mockedJsonData from './db/db.json';
+
 window.Alpine = Alpine;
+Alpine.store('content', mockedJsonData);
 Alpine.start();
