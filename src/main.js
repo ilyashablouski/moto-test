c04e19dfe00f567e0ce0b823519575ac9ce3b0b1
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-08-19 03:30:27 +0300

    add border animation & test scroll dragabble

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import '@styles/components.styles.js';
 import Alpine from 'alpinejs';
 
 import mockedJsonData from './db/db.json';
-import animateBorder from './components/page/alpine.js';
+import animateBorder from './animations/animateBorder';
+import horizontalScrollWithDrag from './animations/horizontalScrollWithDrag';
 
 window.Alpine = Alpine;
 Alpine.store('content', mockedJsonData);
 
 //Components registration
 Alpine.data('animateBorder', animateBorder);
+Alpine.data('horizontalScrollWithDrag', horizontalScrollWithDrag);
+
 Alpine.start();
